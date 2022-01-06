c2aa86d02271fc4126bf4fe02dcb65df1d32a42d
Zach <88895551+ZachJRichman@users.noreply.github.com>
2022-01-06 20:35:54 +0000

    Beta Almost Complete!

--- js/name.js
+++ js/name.js
@@ -1,6 +1,6 @@
-const setName = () => {
+/* const setName = () => {
   const e = document.getElementById('input').value;
   for (let i = 0; i < 2; i++){
     document.getElementById('name').value = e;
   } 
-}
+} */
